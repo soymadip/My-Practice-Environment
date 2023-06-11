f3792a5e000cd67ece9edc47a6c22a3fabf6b2ba
SD <soumadip377@gmail.com>
2023-06-11 12:04:36 +0530

    make practice...

---
+++ PYTHON/EXERCISE#1: practice module.py
@@ -0,0 +1,70 @@
+##created from @codewithharry's replit
+
+##write answer in the next line of the comments.
+##LIKE- 
+a = "example string" 
+#create title
+print(a.title())
+
+
+
+
+
+
+a = "!!!Harry!! !!!!!!!!! Harry"
+print(len(a))
+print(a)
+#upppercase-
+
+#lowercase
+#strip space- 
+#repalace harry with john
+#split from space
+a1 = "harry+john"
+#split from +
+blogHeading = "introduction tO jS"
+#capitalize
+
+str1 = "Welcome to the Console!!!"
+print(len(str1))
+#align the string "str1" to the center 50 
+#the number of times "Harry" has occurred within
+
+str1 = "Welcome to the Console !!!"
+#check if str1 ends with !!!
+
+str1 = "Welcome to the Console !!!"
+#check if str1 ends with !!! from 4 to 10
+
+str1 = "He's name is Dan. He is an honest man."
+#use index to find "ishh"
+#use find to find "ishh"
+
+str1 = "WelcomeToTheConsole"
+#check if number and alphabet
+str1 = "Welcome"
+#check if only alphabet
+
+str1 = "hello world"
+#check if only lower
+
+str1 = "We wish you a Merry Christmas\n"
+#check if printable
+
+str1 = "         "       #using Spacebar
+#check if it has space
+
+str1 = "World Health Organization" 
+#check if it is Title(1st letters are capital)
+
+str2 = "To kill a Mocking bird"
+#check if title
+
+str1 = "Python is a Interpreted Language" 
+#check if the sentence starts with python
+
+str1 = "Python is a Interpreted Language" 
+#change the character casing of the string. Convert Upper case to lower case and lower case to upper case.
+
+str1 = "His name is Dan. Dan is an honest man."
+#convert to titleformat

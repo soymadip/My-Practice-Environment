5aa765e5d121a19b2f01e10edf8821bc8a0d79ed
SD <soumadip377@gmail.com>
2023-06-15 13:25:25 +0530

    do while loop

--- PRACTICE.py
+++ PRACTICE.py
@@ -1,28 +1,12 @@
-x = int(input('enter bro: '))
-
-print('here is if-else result:')
-if (x==0):
-    print('x is zero')
-elif(x==4):
-    print('case is 4')
-elif(x!=90):
-    print(x,'is not 90')
-elif(x!=80):
-    print(x,'is not 80')
-else:
-    print(x)
 
 
 
-print('here is match case result:')
-match x:
-    case 0:
-        print('x is zero')
-    case 4:
-        print('case is 4')
-    case _ if x !=90:
-        print(x,'is not 90')
-    case _ if x !=80:
-        print(x,'is not 80')
-    case _:
-        print(x)
+# do while loop
+
+while True:
+    i = int(input('enter a number more than 100: '))
+    if(i<100):
+        print('this is not more than 1oo')
+        continue
+    print('congratulations dood')
+    break

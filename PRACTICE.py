932a6cf60fe4f3d53d281e30a42c23a85d386ee2
SD <soumadip377@gmail.com>
2023-06-16 17:38:03 +0530

    ..

--- PRACTICE.py
+++ PRACTICE.py
@@ -1,16 +1,4 @@
 
 
 
-# do while loop
-
-while True:
-    i = int(input('enter a number more than 100: '))
-    if(i<100):
-        print('this is not more than 1oo')
-        continue
-    print('congratulations dood')
-    break
-
-
-
 

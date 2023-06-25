00695465354189ed956d02eb01f579edb72eeeef
SD <soumadip377@gmail.com>
2023-06-25 19:45:25 +0530

    learned how to use function callout in f-string

--- PYTHON/PROJECT#2 . Factorial & Fibonacci Number.py
+++ PYTHON/PROJECT#2 . Factorial & Fibonacci Number.py
@@ -1,3 +1,6 @@
+# factorial & fibonacci number calculator
+
+#----------------------------------------------------
 
 # function to calculate factorial:
 
@@ -8,39 +11,41 @@ def factorial(n): # this thing has a limit of 999 though😁
         return n*factorial(n-1)
 
 
-# function to calculate Fibonassi number:
-# it works like this: 
+# function to calculate fibonacci number:
+# it works like this:
+# f(0) = 0, f(1) = 1, f(n) = f(n-1) + f(n-2) # f represents fibonacci itself
 # f(0) = 0, f(1) = 1, f(2) = 1+0 = 1, f(3) = 2+1 = 3, f(4) = 3+2 = 5, f(5) = 4+3 = 7 
-# f(0) = 0, f(1) = 1, f(n) = f(n-1) + f(n-2)
 
-def fibonassi(n):
+
+def fibonacci(n):
     if(n == 1):
         return 1
     elif(n == 0):
         return 0
     else:
-        return ((n-1) + (n-2))
+        return fibonacci(n-1) + fibonacci(n-2)
 
 # function to calculate both- 
 
-def calculator(input, function):
-    '''i am geting brackes at my output. gotta fix it later''' # sucks to be a beginner!!
-    if(input >= 999):
+def calculator(input_num, function):
+    if(input_num > 999):
         return('sorry sir my limit is 999.\nI can not calculate from 4 digit numbers..')
-    elif(function == 1):
-        return(f'fibonassi number of {input} is:', fibonassi(input))
+    if(function == 1):
+        return (f'fibonacci number of {input_num} is: {fibonacci(input_num)}')
     elif(function == 2):
-        return(f'Factorial of {input} is',factorial(input))
+        return (f'Factorial of {input_num} is {factorial(input_num)}')
     elif(function == 3):
-        return(f'Factorial of {input} is',factorial(input),f'\nFibonassi number of {input} is',fibonassi(input))
+        return (f'Factorial of {input_num} is {factorial(input_num)}\nfibonacci number of {input_num} is {fibonacci(input_num)}')
     else:
-        return(f'your input {input} is not in my supported functions')
+        return (f'your input {input_num} is not in my supported functions')
 
-# Finally input & output:
+# Finally input_num & output:
 
-function = int(input('''Here is my supported functions:\nPress 1 to calculate "Fibonassi Number".\n Press 2 to calculate "Factorial" of a number.\n Press 3 to calculate Both.\nGimme input according to What you wanna calculate:'''))
-input = int(input('Now give me the number you wanna calculate2:'))
-print(calculator(input, function))
+function = int(input('''Here are the supported functions:
+Press 1 to calculate the "Fibonacci Number".
+Press 2 to calculate the "Factorial" of a number.
+Press 3 to calculate both.
+Enter your choice: '''))
+input_num = int(input('Now give me the number you wanna calculate:'))
 
-
-
+print(calculator(input_num, function))

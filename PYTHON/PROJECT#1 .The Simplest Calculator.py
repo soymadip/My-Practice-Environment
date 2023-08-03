bc38331337cba078eb939852d5fbbaafab2d9431
SD <soumadip377@gmail.com>
2023-08-03 23:30:08 +0530

    ..

--- PYTHON/PROJECT#1 .The Simplest Calculator.py
+++ PYTHON/PROJECT#1 .The Simplest Calculator.py
@@ -1,6 +1,8 @@
 a = input("Type your 1st number: ")
 b = input("Type your 2nd number: ")
 c = input("Type your 3rd number: ")
+
+
 # d1 = int(a)+ int(b)+ int(c)
 # d2 = int(a)- int(b)- int(c)
 # d3 = int(a)* int(b)* int(c)
@@ -8,6 +10,7 @@ c = input("Type your 3rd number: ")
 # d5 = int(a)% int(b)
 # d6 = int(a)** int(b)** int(c)
 # d7 = int(a)// int(b) #cannot use in the simpliest calcuulator.😁
+
 
 print("THe value of",a,"+",b,"+",c,"is:",int(a)+ int(b)+ int(c))
 print("THe value of",a,"-",b,"-",c,"is:",int(a)- int(b)- int(c))
